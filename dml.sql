e11a4b6a9957f8e875af3b8cfdf28745885fdc68
zhou <2997152550@qq.com>
2020-08-21 22:36:34 +0800

    modified

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+INSERT INTO tablename(field1,field2,...,fieldn) VALUES(value1,value2,...,valuen);
 -- 修改记录  
-
+UPDATE tablename SET field1=value1 [WHERE CONDITION];
 --  删除记录  
-
+delete from tablename [WHERE CONDITION];
 -- 查询记录  
+SELECT * FROM tablename [WHERE CONDITION];
